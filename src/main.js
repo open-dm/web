7eb8bfddcd8c26b1e5b98f3ed44dbc60133e4d1e
George Broadhurst <george@rapanuiclothing.com>
2020-02-12 20:22:41 +0000

    chore: install element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
+import './assets/style/core.scss';
+import './assets/tailwind.css';
+
 
 Vue.config.productionTip = false;
+
+Vue.use(Element);
 
 new Vue({
   render: (h) => h(App),
