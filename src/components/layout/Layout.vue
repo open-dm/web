a295a0f426098286149f7349a187ec02ffec7a3b
George Broadhurst <george@rapanuiclothing.com>
2020-02-12 20:23:10 +0000

    feat: general layout

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-container class="wrapper">
+    <sidebar />
+
+    <el-container>
+
+      <el-header class="header">
+        <div class="logo-container">
+          <img
+            class="inline-block"
+            alt="OpenDM logo"
+            src="@/assets/vectors/opendmlogo.svg"
+            width="30px"
+          >
+          <span>Open DM</span>
+        </div>
+      </el-header>
+
+      <el-main class="body">
+        <el-button type="primary">Primary</el-button>
+        <el-button type="secondary">Secondary</el-button>
+      </el-main>
+
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Sidebar from './Sidebar.vue';
+
+export default {
+  name: 'layout',
+
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.logo-container {
+  padding: 1em;
+
+  img {
+    margin-right: 0.5em;
+  }
+}
+
+.wrapper {
+  @apply h-screen;
+}
+
+.header {
+  @apply shadow;
+}
+
+.body {
+  @apply h-full;
+}
+</style>
---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-aside
+    width="200px"
+    class="side-bar"
+  >
+    <el-menu class="menu">
+      <el-menu-item>Characters</el-menu-item>
+      <el-menu-item>Monsters</el-menu-item>
+      <el-menu-item>Items</el-menu-item>
+      <el-menu-item>Journal</el-menu-item>
+      <el-menu-item>Locations</el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+};
+</script>
+
+<style scoped>
+.side-bar,
+.menu * {
+  @apply bg-blue-600 text-white;
+}
+
+
+.menu *:hover {
+  @apply bg-gray-300 text-blue-700;
+}
+</style>
